3ae9f9fdcfcf88143f5c37bf6e4b79a300d0135c
NJU-ymhui <132819158+NJU-ymhui@users.noreply.github.com>
2024-01-18 16:53:16 +0800

    Update Transformer.java
    改bug(可能还有 ）

--- src/Transformer.java
+++ src/Transformer.java
@@ -3,14 +3,34 @@
 public class Transformer {
     public static int toInteger(byte[] bytes) {
         int ans = 0;
-        for (int i = 0; i < bytes.length; i++) ans += bytes[bytes.length - 1 - i] * (1 << (i * 8));
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[bytes.length - 1 - i];
+            ans += (b & 0xff) << (i * 8);
+        }
         return ans;
     }
+//    public static void main(String[] s) {
+//        byte[] b = new byte[]{(byte) 0b00000001, 0b00110011};
+//        System.out.println(toInteger(toBytes(toInteger(b), 2)));
+//    }
+
     public static byte[] toBytes(int number, int bytes) {
         byte[] res = new byte[bytes];
-        res[bytes - 1] = (byte) (number % (1 << 7));
-        for (int i = 0; i < bytes - 1; i++) {
-            res[i] = (byte) (number % (1 << (8 * (bytes - i) - 1)) / (1 << (8 * (bytes - i - 1) - 1)));
+//        res[bytes - 1] = (byte) (number % (1 << 7));
+//        for (int i = 0; i < bytes - 1; i++) {
+//            res[i] = (byte) (number % (1 << (8 * (bytes - i) - 1)) / (1 << (8 * (bytes - i - 1) - 1)));
+//        }
+        for (int i = bytes - 1; i >= 0; i--) {
+            int tmp = number % (1 << 8);
+            number /= 1 << 8;
+            int j = 0;
+            while (j < 8) {
+                if (tmp % 2 == 1) {
+                    res[i] = (byte) (res[i] | (1 << j));
+                }
+                j++;
+                tmp /= 2;
+            }
         }
         return res;
     }
