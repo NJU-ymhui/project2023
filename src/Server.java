a53e6d795209c938d9087eb363826fc34e21b0fc
NJU-ymhui <132819158+NJU-ymhui@users.noreply.github.com>
2024-01-18 16:49:47 +0800

    Update Server.java
    合并基础功能，主函数里加了握手与数据据传输，挥手暂时没有验证但是应该是有bug的

--- src/Server.java
+++ src/Server.java
@@ -4,10 +4,10 @@ import java.net.Socket;
 import java.util.Random;
 
 public class Server extends Client{
-    public int MSS = 0; // 报文可承载的最大数据量
     private boolean up = false; //标记服务端是否已启用
     private boolean connected = false;
-    private ServerSocket welcomeSocket;
+
+    //设置一个接受缓冲区
 
     /**
      * 根据当前报文中window值设置window大小
@@ -20,6 +20,7 @@ public class Server extends Client{
      * */
     public void start() throws Exception{
         up = true;
+        sentPacket = receivedPacket = null;
         welcomeSocket = new ServerSocket(port);
         System.out.printf("Server started! Listening on %d...\n", port);
     }
@@ -28,6 +29,18 @@ public class Server extends Client{
      * */
     public void close() {
         up = false;
+    }
+    public void release() throws Exception{
+        connected = false;
+        connectionSocket = null;
+        receivedPacket = sentPacket = null;
+        //welcomeSocket.close();
+        System.out.println("release connection with client");
+        start();
+        System.out.println();
+    }
+    public boolean connect() {
+        return connected;
     }
     /***
      * 对服务器状态作检查，如是否处于启用状态，监听端口是否为合法端口（-1即不合法端口）等
@@ -47,53 +60,62 @@ public class Server extends Client{
     private static void buildConnection(Server server) throws Exception{
         while (!server.connected) {
             //监听以接收报文
-            Socket listenSocket = server.welcomeSocket.accept();// 先主动建立一个用于接收初始请求报文的socket，只用于建立连接前的冷启动
+            server.connectionSocket = server.welcomeSocket.accept();// 先主动建立一个用于接收初始请求报文的socket，只用于建立连接前的冷启动
+            Socket listenSocket = server.connectionSocket;
             InputStream bytesFromClient = listenSocket.getInputStream();// 获取client的字节流
             OutputStream bytesToClient = listenSocket.getOutputStream();
-            byte[] buffer = server.getBytes(bytesFromClient, 20);// buffer中存放client发来报文的字节形式
+            byte[] buffer = server.getBytes(bytesFromClient, 24);// buffer中存放client发来报文的字节形式
             Packet firstHandShake = server.buildPacket(buffer);// 组装报文
             server.receive(firstHandShake);// 收到client的第一次握手报文
+            System.out.println("first from client");
             server.print();// 回显报文
 
-            if (server.checkSYN(firstHandShake) && !server.checkACK(firstHandShake)) {
+            if (server.checkSYN(firstHandShake)) {
+                System.out.println("start ack first handshake");
                 //应答，作为第二次握手的报文
-                int seq = new Random().nextInt(1234567890);// 该报文第一个字节id
+                int seq = new Random().nextInt(123456789);// 该报文第一个字节id
                 byte[] id = Transformer.toBytes(seq, 4);
                 int ack = Transformer.toInteger(server.receivedPacket.getId()) + 1;// 该报文ack应为上一个报文id + 1
+//                System.out.printf("ack=%d\n", ack);
                 byte[] Ack = Transformer.toBytes(ack, 4);
                 byte[] spcBytes = new byte[2];
                 spcBytes[0] = server.receivedPacket.getSpcBytes()[0];
                 spcBytes[1] = (byte) (server.receivedPacket.getSpcBytes()[1] | (1 << 4));// ACK置1
-                int checkSum = 0;// todo calculate checkSum
+                int checkSum = 0;
                 Packet secondShakeHand = new Packet(
                         server.receivedPacket.getDest(), server.receivedPacket.getSrc(), id, Ack,
                         spcBytes, server.receivedPacket.getWindow(), Transformer.toBytes(checkSum, 2),
                         server.receivedPacket.getUrgent(), server.receivedPacket.getOptions(),
-                        server.receivedPacket.getAlign(), server.receivedPacket.MSS
+                        server.receivedPacket.getAlign(), server.MSS
                 );
+//                System.out.println("Server MSs = " + secondShakeHand.MSS);
+                System.out.println("second from server:");
+                System.out.println(secondShakeHand);
                 server.send(listenSocket, secondShakeHand);// 将报文发送给client，完成应答
 
                 //检查第三次握手报文
                 bytesFromClient = listenSocket.getInputStream();
-                buffer = server.getBytes(bytesFromClient, 20);
+                buffer = server.getBytes(bytesFromClient, 24);
                 Packet thirdHandShake = server.buildPacket(buffer);
                 server.receive(thirdHandShake);
+                System.out.println("third from client:");
                 server.print();
                 int clintSeq = Transformer.toInteger(server.receivedPacket.getId()), clientAck = -1;
                 if (server.checkACK(thirdHandShake)) {
                     clientAck = Transformer.toInteger(server.receivedPacket.getAck());
-                }else {
-                    System.out.println("Failed to get connected.");
+                } else {
+                    System.out.println("Failed to get connected because of ACK=0.");
                     continue;
                 }
-                if (!server.checkSYN(thirdHandShake) && clintSeq == ack && clientAck == seq + 1) {
+                if (clintSeq == ack && clientAck == seq + 1) {
                     // 符合第三次握手的报文规范，可以建立连接
                     server.connected = true;
                     System.out.println("Connected to Client.");
                     server.setWindowSize();
                     server.window.initPackets();
-                }else {
-                    System.out.println("Failed to get connected.");
+                } else {
+                    System.out.printf("clientSeq=%d, ack=%d, clientAck=%d, seq=%d\n", clintSeq, ack, clientAck, seq);
+                    System.out.println("Failed to get connected because of ACK or ack.");
                     //告诉client连接失败，send RST=1
                     spcBytes[1] = (byte) (spcBytes[1] | (1 << 2));// RST置1
                     Packet failure = new Packet(
@@ -103,19 +125,86 @@ public class Server extends Client{
                             server.receivedPacket.getAlign(), server.MSS
                     );
                     bytesToClient.write(failure.getBytes()); // 通知client连接失败
-                    listenSocket.close();
-                    server.welcomeSocket.close();
                 }
+            } else {
+                System.out.println("wrong when server try to build connection");
+                continue;// 对于未建立连接时的一切SYN != 1 || ACK != 0的报文直接丢弃}
             }
-            else continue;// 对于未建立连接时的一切SYN != 1 || ACK != 0的报文直接丢弃
+//            bytesFromClient.close();
+//            bytesToClient.close();
         }
     }
     /**
      * 挥手释放连接
      * */
-    private static void releaseConnection(Server server) throws Exception{
-        //TODO 挥手释放连接
+    private static void releaseConnection(Server server, Socket connectingSocket) throws Exception {
+        //  Socket connectingSocket = server.welcomeSocket.accept();//连接中的socket
+        InputStream bytesFromClient = connectingSocket.getInputStream();
+        OutputStream bytesToClient = connectingSocket.getOutputStream();//暂未使用
+        byte[] buffer = server.getBytes(bytesFromClient, 24);
+        Packet firstHandShake = server.buildPacket(buffer);// 组装报文
+        server.receive(firstHandShake);// 收到client的第一次挥手报文
+        server.print();// 回显报文
+
+        if (server.checkFIN(firstHandShake)) { //检查第一次挥手报文
+            //应答，作为第二次挥手的报文
+            int seq = new Random().nextInt(123456789);// 该报文第一个字节id
+            byte[] id = Transformer.toBytes(seq, 4);
+            int ack = Transformer.toInteger(server.receivedPacket.getId()) + 1;// 该报文ack应为上一个报文id + 1
+            byte[] Ack = Transformer.toBytes(ack, 4);
+            byte[] spcBytes = new byte[2];
+            spcBytes[0] = server.receivedPacket.getSpcBytes()[0];
+            spcBytes[1] = (byte) (server.receivedPacket.getSpcBytes()[1] | (1 << 4));// ACK置1
+            spcBytes[1] = (byte) (spcBytes[1] & 0b11111110);// FIN置0
+
+            int checkSum = 0;
+            Packet secondShakeHand = new Packet(
+                    server.receivedPacket.getDest(), server.receivedPacket.getSrc(), id, Ack,
+                    spcBytes, server.receivedPacket.getWindow(), Transformer.toBytes(checkSum, 2),
+                    server.receivedPacket.getUrgent(), server.receivedPacket.getOptions(),
+                    server.receivedPacket.getAlign(), server.receivedPacket.MSS
+            );
+            server.send(connectingSocket, secondShakeHand);// 将报文发送给client，完成应答
+            //todo:data transfer
+            // 第二次挥手报文发送完毕，client断开连接，server将剩下的数据传输完毕；
+
+            //数据传输完毕，组装第三次挥手报文
+            seq = new Random().nextInt(123);// todo：该报文第一个字节id，应为传输数据完毕后的下一个序号，暂时填充为随机
+            id = Transformer.toBytes(seq, 4);
+            spcBytes[1] = (byte) (spcBytes[1] | 0b00000001);
+            //更换seq，FIN置回1，其它部分报文不变
+            Packet thirdHandShake = new Packet(
+                    server.receivedPacket.getDest(), server.receivedPacket.getSrc(), id, Ack,
+                    spcBytes, server.receivedPacket.getWindow(), Transformer.toBytes(checkSum, 2),
+                    server.receivedPacket.getUrgent(), server.receivedPacket.getOptions(),
+                    server.receivedPacket.getAlign(), server.receivedPacket.MSS
+            );
+
+            while (server.connected) {
+                server.send(connectingSocket, thirdHandShake);// 将报文发送给client，请求断开连接；
+                //接受第四次挥手报文
+                bytesFromClient = connectingSocket.getInputStream();
+                buffer = server.getBytes(bytesFromClient, 24);
+                Packet fourthHandShake = server.buildPacket(buffer);
+                server.receive(fourthHandShake);
+                server.print();
+                //回显报文
+                if (server.checkACK(fourthHandShake) &&
+                        Transformer.toInteger(server.receivedPacket.getId()) == ack + 1 &&
+                        Transformer.toInteger(server.receivedPacket.getAck()) == seq + 1) {
+                    //符合挥手规范，释放连接
+                    server.connected = false;
+                    System.out.println("Connection released.");
+                    connectingSocket.close();
+                } else {
+                    System.out.println("Failed to release connection.");
+                }//不符合挥手规范，释放连接失败
+            }
+        } else {
+            System.out.println("Failed to release connection.");
+        }//不符合挥手规范，释放连接失败，抛出异常
     }
+
 
     public Server() {
         super();
@@ -126,7 +215,6 @@ public class Server extends Client{
         //argv[0]为服务器启动时监听的端口号; argv[1] argv[2]分别为传输文件的path和MSS(需要判断是否存在)
         //todo
         Server server = new Server();
-
         if (argv.length != 3) {
             System.out.println("Parameter count error!");
             return;
@@ -143,7 +231,8 @@ public class Server extends Client{
             return;
         }// 端口号是否合法
 
-        File file = new File(argv[1]);
+        String path = argv[1];
+        File file = new File(path);
         try (FileInputStream inputFromFile = new FileInputStream(file)){
             ;
         }catch (FileNotFoundException e) {
@@ -165,20 +254,32 @@ public class Server extends Client{
         server.start();
 
         while (server.check()) {
-            //todo 执行服务器逻辑
-            //byte[] d = new byte[114];
-            while (true) {
+            //执行服务器逻辑
+            while (!server.connect()){
                 try {
                     Server.buildConnection(server);
                     break;
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    System.out.println("Connected failed.");
                 }
-            }// finish connecting
-            Socket connectionSocket = server.welcomeSocket.accept();
-            //todo Data Transfer
-
-            //todo release
+            }
+            //finish connecting
+            Socket connectionSocket = server.connectionSocket;
+            //Data Transfer
+            while (connectionSocket.isConnected()){
+                try {
+//                    String hello = "hello";
+//                    OutputStream o = connectionSocket.getOutputStream();
+//                    o.write(hello.getBytes());
+//                    o.flush();
+                    Server.dataTransfer(server, connectionSocket, path);
+                }catch (Exception e) {
+                    server.release();
+//                    return;
+                    break;
+                }
+            }
+            //todo release connection
         }
         server.close();
         System.out.println("Server shut down unexpectedly.");
